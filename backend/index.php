408a1140ebe5e1dcced36ea81a40876911395433
Nikhil Singh <singhnikhilbtp@gmail.com>
2026-01-06 03:44:42 +0530

    Add index entry point for authentication flows

---
+++ backend/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Cognito Auth LAMP Stack</h2>";
+echo "<ul>
+        <li><a href='../frontend/signup.html'>Signup</a></li>
+        <li><a href='../frontend/confirm.html'>Confirm Signup</a></li>
+        <li><a href='../frontend/login.html'>Login</a></li>
+      </ul>";
